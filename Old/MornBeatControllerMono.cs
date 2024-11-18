c03b3b5673787a07b7c0b1d256efac41b52b0fd4
matsufriends <pikapikan0526@gmail.com>
2024-11-19 03:54:54 +0900

    ReadOnlyAttributeが単体で動作するように

--- Old/MornBeatControllerMono.cs
+++ Old/MornBeatControllerMono.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using MornAttribute;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Assertions;
---
+++ Util/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace MornBeat
+{
+    internal sealed class ReadOnlyAttribute : PropertyAttribute
+    {
+    }
+}
