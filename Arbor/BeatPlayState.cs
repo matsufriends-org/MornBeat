8f51277af50e341033472c22d4cccfd9be4aab4d
matsufriends <pikapikan0526@gmail.com>
2025-06-25 06:51:59 +0900

    Beat再生を独立で可能に

--- Arbor/BeatPlayState.cs
+++ Arbor/BeatPlayState.cs
@@ -1,5 +1,6 @@
 ﻿#if USE_ARBOR
 using Arbor;
+using MornUtil;
 using UnityEngine;
 using VContainer;
 
@@ -8,16 +9,18 @@ namespace MornBeat
     internal class BeatPlayState : StateBehaviour
     {
         [SerializeField] private MornBeatMemoSo _beatMemo;
+        [SerializeField] private bool _executeIsolated;
         [SerializeField] private StateLink _onComplete;
         [Inject] private MornBeatControllerMono _beatController;
 
         public override async void OnStateBegin()
         {
+            var ct = _executeIsolated ? MornApp.QuitToken : CancellationTokenOnEnd;
             await _beatController.StartAsync(
                 new MornBeatStartInfo
                 {
                     BeatMemo = _beatMemo,
-                    Ct = CancellationTokenOnEnd,
+                    Ct = ct,
                 });
             Transition(_onComplete);
         }
